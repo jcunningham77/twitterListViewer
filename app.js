55140b976f288dbcede74f6eebf30085499e3afa
Jeffrey Cunningham <jcunningham77@gmail.com>
2016-09-09 18:22:09 -0400

    set up web server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// var express = req
+ var express = require('express');
+ var app =express();
+ var port = process.env.PORT || 3000;
+
+ app.use('/assets/',express.static(__dirname+'/public'));
+
+ app.listen(port);
