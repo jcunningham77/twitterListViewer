ff641a87e8522329f82b16f7b1f1bebc678d147c
Jeffrey Cunningham <jcunningham77@gmail.com>
2016-10-31 18:53:12 -0400

    update port to use openshift's config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  var mongoose = require('mongoose');
  var config = require('./config');
  var app =express();
- var port = process.env.PORT || 3000;
+ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
  var twitterController = require('./controller/twitterController');
  var authController = require('./controller/authController');
 
