e17214c2859272834fe05f728e46218921fe729b
Jeffrey Cunningham <jcunningham77@gmail.com>
2016-11-01 20:01:11 -0400

    fix IP

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  var config = require('./config');
  var app =express();
  var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
- var ip = process.env.OPENSHIFT_NODEJS_PORT || 'localhost';
+ var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
  var twitterController = require('./controller/twitterController');
  var authController = require('./controller/authController');
 
