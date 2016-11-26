059ebd6e69e7a4d05e09f0b14b217fdc594e30bb
Jeff Cunningham <jcunningham77@gmail.com>
2016-11-25 19:56:37 -0500

    add ngAnimate dependency

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	angular.module('twitterListViewer',['ngRoute','ngCookies','ngSanitize', 'ui.bootstrap'])
+	angular.module('twitterListViewer',['ngRoute','ngCookies','ngSanitize', 'ui.bootstrap','ngAnimate'])
 	.config(function($routeProvider, $locationProvider){
 		$routeProvider
 
